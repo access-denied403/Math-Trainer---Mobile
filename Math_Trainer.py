a11ea6a6eb8b9b95574642398a328555129768e5
Access.denied.403 <50791042+access-denied403@users.noreply.github.com>
2020-06-30 14:27:58 +0200

    Add files via upload

---
+++ Math_Trainer.py
@@ -0,0 +1,93 @@
+import kivy
+import random
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.uix.popup import Popup
+from kivy.uix.screenmanager import ScreenManager, Screen
+kivy.require('1.11.1')
+
+class Home_page(Screen):
+    def addition(self):
+        screen_manager.current = "add"
+
+    def subtraction(self):
+        screen_manager.current = "sub"
+
+    def multiplication(self):
+        screen_manager.current = "mul"
+
+    def division(self):
+        screen_manager.current = "div"
+
+
+class Add_page(Screen):
+    
+    answer = ObjectProperty(None)
+    question = ObjectProperty(None)
+    score = ObjectProperty(None)
+    
+    def __init__(self, **kwargs):
+    	super().__init__(**kwargs)
+    	num1 = random.randint(1, 9)
+    	num2 = random.randint(1, 9)
+    	self.question.text = self.ask()
+
+
+    def ask(self):
+    	self.num1 = random.randint(1, 9)
+    	self.num2 = random.randint(1, 9)
+    	return f"{self.num1} + {self.num2}"
+    
+    
+    def submit(self):
+    	if str(self.answer.text) == str(self.num1 + self.num2):
+    		self.score.text = str(int(self.score.text) + 1)
+    		self.answer.text = ""
+    		self.question.text = self.ask()
+    		
+    	else:
+    		self.score.text = str(int(self.score.text) - 1)
+    		self.answer.text = ""
+    		self.question.text = self.ask()
+    	 
+    def home(self):
+        screen_manager.current = "home"
+
+
+class Sub_page(Screen):
+    def home(self):
+        screen_manager.current = "home"
+
+
+class Mul_page(Screen):
+    def home(self):
+        screen_manager.current = "home"
+
+
+class Div_page(Screen):
+    def home(self):
+        screen_manager.current = "home"
+
+
+graphics = Builder.load_file("Math_Trainer.kv")
+screen_manager = ScreenManager()
+
+screens = [Home_page(name="home"), Add_page(name="add"), Sub_page(name="sub"), Mul_page(name="mul"), Div_page(name="div")]
+for screen in screens:
+    screen_manager.add_widget(screen)
+
+screen_manager.current = "home"
+
+
+class Math_trainer(App):
+    def build(self):
+        return screen_manager
+
+if __name__ == '__main__':
+    Math_trainer().run()
